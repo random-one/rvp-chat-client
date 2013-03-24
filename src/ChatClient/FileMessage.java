525bee54bfef4e4f7da405d7c654bdf8b2ec1441
Radina Staneva <radistaneva@gmail.com>
2013-03-24 15:10:06 +0200

    Added getters and setters

--- src/ChatClient/FileMessage.java
+++ src/ChatClient/FileMessage.java
@@ -5,6 +5,19 @@ public class FileMessage extends Message implements java.io.Serializable {
 	private static final long serialVersionUID = 1L;
 	private String fileName;
 	private byte[] buffer;
+	//TODO: add compareTo to determine if a file with this name is transfered
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public byte[] getBuffer() {
+		return buffer;
+	}
 
 	public FileMessage(String sender, String receiver, String fileName) {
 		super(sender, receiver, msgType.FILE_MESSAGE);
