3500e87ea588d91689803d94924581ec89fab10a
Radina Staneva <radistaneva@gmail.com>
2013-03-24 14:10:44 +0200

    Added TextMessage class

---
+++ src/ChatClient/TextMessage.java
@@ -0,0 +1,20 @@
+package ChatClient;
+
+public class TextMessage extends Message implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String content;
+
+	public TextMessage(String sender, String receiver, String content) {
+		super(sender, receiver, msgType.TEXT_MESSAGE);
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}	
+}
