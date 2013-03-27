fc0c42ba4432181b40941868efd4c219c473f6e2
Radina Staneva <radistaneva@gmail.com>
2013-03-28 01:13:34 +0200

    Added new ClientConnection class that establishes the client-server connection

---
+++ src/ChatClient/ClientConnection.java
@@ -0,0 +1,37 @@
+package ChatClient;
+import java.net.Socket;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+
+public class ClientConnection extends Thread {
+	private Socket request;
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
+
+	ClientConnection(Socket client) throws Exception 
+	{
+		request = client;
+		in=new ObjectInputStream(request.getInputStream());
+		out=new ObjectOutputStream(request.getOutputStream());
+
+		ServerSide.clients.add(request);
+		start();
+	}
+
+	public void run()
+	{
+		while (true)
+		{
+			try {
+				Message message = (Message)in.readObject();
+				System.out.println("received from client");
+				if (message.getType() == Message.msgType.TEXT_MESSAGE) {
+					TextMessage tm = (TextMessage)message;
+					System.out.println("server sent: '" + tm.getContent() + "' to " + tm.getReceiver());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
