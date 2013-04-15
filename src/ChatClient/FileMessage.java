a4fc54c4dd32942e6bf5ff2df2bcb375c6f2ac8a
Radina Staneva <radistaneva@gmail.com>
2013-04-15 22:27:44 +0300

    FileMessage class improved

--- src/ChatClient/FileMessage.java
+++ src/ChatClient/FileMessage.java
@@ -5,8 +5,12 @@ public class FileMessage extends Message implements java.io.Serializable, P2PMes
 
 	private static final long serialVersionUID = 1L;
 	private String fileName;
-	private byte[] buffer;
-	//TODO: add compareTo to determine if a file with this name is transfered
+	private int size;
+	private byte[] buffer = new byte[10240];
+
+	public FileMessage() {
+		setType(Message.msgType.FILE_MESSAGE);
+	}
 
 	public String getFileName() {
 		return fileName;
@@ -20,8 +24,15 @@ public class FileMessage extends Message implements java.io.Serializable, P2PMes
 		return buffer;
 	}
 
-	public FileMessage(String sender, String receiver, String fileName) {
-		super(sender, receiver, msgType.FILE_MESSAGE);
-		this.fileName = fileName;
+	public void setBuffer(byte[] buffer) {
+		this.buffer = buffer;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
 	}
 }
